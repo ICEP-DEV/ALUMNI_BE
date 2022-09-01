95b272a96ae9ef5008e1b937d186c78f227e865f
itslezley <stilesunarine1@gmail.com>
2022-09-01 09:38:50 +0200

    The registration api

---
+++ Registration_api/index.js
@@ -0,0 +1,20 @@
+const mysql =require('mysql');
+const express = require('express');
+var app = express();
+const bodyparser = require('body-parser');
+const cors = require('cors');
+
+//files
+const add=require('./routes/add_alumni');
+
+//instantiating 
+app.use(cors());
+app.use(express.json());
+app.use(bodyparser.json());
+
+//go to add_alumni
+app.use('/register',add);
+
+app.listen(3000,()=>console.log('Server is running......'));
+
+
