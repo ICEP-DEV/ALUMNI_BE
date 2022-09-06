d52d45c3ef449570293bc9f6144f126b5063001a
Refilwe58 <107126000+Refilwe58@users.noreply.github.com>
2022-09-06 09:55:40 +0200

    Add files via upload

---
+++ chats.js
@@ -0,0 +1,64 @@
+
+const express = require('express');const bodyparser=require('body-parser');
+const cors = require('cors');
+const mysql=require('mysql2');
+const router = express.Router();
+const app=express();
+const login=require('./login');
+
+app.use(cors());
+app.use(bodyparser.json());
+const db = require('./database');
+
+///get all users except the logged in users
+router.get('/get_users', (req, res) => {
+    let userId=req.params.id;
+    let sql = `SELECT alumni_id,alumni_name FROM alumni WHERE alumni_id != '${req.session.Alumni.alumni_id}'`;
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        else {
+            res.status(201).send(result);
+        };
+    });
+});
+
+///send messages
+router.post('/send_messages/:receiverId/', (req, res) => {
+    
+    let receiverId = req.params.receiverId;
+    let senderId =req.session.Alumni.alumni_id;
+    let message = req.body.message;
+    if (message != null && senderId != receiverId) {
+        let sql = `INSERT INTO chat_box(senderId,receiverId,message)
+             VALUES('${senderId}','${receiverId}','${message}')`;
+        //run the query
+
+        db.query(sql, (err, results) => {
+            if (err) return res.status(401).send(+err);
+            else {
+               return res.send('messages sent');
+
+            }
+
+        });
+    } else{
+        res.sendStatus(401);
+    }
+
+});
+
+//get messages
+router.get('/get_message/:id', (req, res) => {
+    receiverId = req.params.id;
+    let sql = `SELECT * FROM chat_box  WHERE senderId='${req.session.Alumni.alumni_id}' AND receiverId='${receiverId}' 
+              OR  senderId='${receiverId}' AND receiverId='${req.session.Alumni.alumni_id}'`;
+    //run query
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        else {
+            res.status(201).send(result);
+        };
+    });
+});
+
+module.exports = router;
