d151412d8b6af1decff4b27d88ea738e4623e14a
Refilwe58 <107126000+Refilwe58@users.noreply.github.com>
2022-10-19 10:21:21 +0200

    changing port to 3100

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const boddParser = require('body-parser')
 const cors = require('cors');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
-const  port = process.env.PORT || 3000;
+const  port = process.env.PORT || 3100;
 
 // files
 const login=require('./routes/login');
