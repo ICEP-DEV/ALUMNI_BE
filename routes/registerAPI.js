f9d2c8c0cea9cce4036dba6462a4310fd4acfa39
Refilwe58 <107126000+Refilwe58@users.noreply.github.com>
2022-10-24 11:39:03 +0200

    Update registerAPI.js

--- routes/registerAPI.js
+++ routes/registerAPI.js
@@ -13,6 +13,10 @@ getRegisterAPI.post('/', (req, res) => {
         connection.query('INSERT INTO alumni SET ?', params, (err, rows) => {
         connection.release() // return the connection to pool
         if (!err) {
+            res.setHeader("Access-Control-Allow-Origin", "*")
+            res.setHeader("Access-Control-Allow-Credentials", "true");
+            res.setHeader("Access-Control-Max-Age", "1800");
+            res.setHeader("Access-Control-Allow-Headers", "content-type");
             res.send({message:'Successfully  Registered'})
         } else {
             console.log(err)
