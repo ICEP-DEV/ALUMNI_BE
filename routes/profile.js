483c9309b9400014010057588275a01e14d050de
Refilwe58 <107126000+Refilwe58@users.noreply.github.com>
2022-11-01 15:44:15 +0200

    auth

--- routes/profile.js
+++ routes/profile.js
@@ -3,6 +3,8 @@ const bodyparser=require('body-parser');
 const cors = require('cors');
 const mysql=require('mysql2');
 const router = express.Router();
+const auth = require('./auth');
+const JWT= require('jsonwebtoken')
 const app=express();
 const session = require('express-session');
 
@@ -12,7 +14,7 @@ app.use(bodyparser.json());
 const database=require('../Database/database.js');
 const cookieParser = require('cookie-parser');
 
-router.get('/',(req, res)=>{
+router.get('/',auth,(req, res)=>{
 
 try{
 return res.status(200).send({Name:req.session.Alumni.alumni_name
