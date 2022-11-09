b58d4368f292083818714356b38cb11ad9a960bf
Refilwe58 <107126000+Refilwe58@users.noreply.github.com>
2022-11-09 09:50:51 +0200

    adding auth to the profile path

--- routes/profile.js
+++ routes/profile.js
@@ -7,6 +7,7 @@ const auth = require('./auth');
 const JWT= require('jsonwebtoken')
 const app=express();
 const session = require('express-session');
+const jwtMiddleware=require('./jwtMiddleware')
 
 app.use(cors());
 app.use(bodyparser.json());
@@ -14,14 +15,22 @@ app.use(bodyparser.json());
 const database=require('../Database/database.js');
 const cookieParser = require('cookie-parser');
 
-router.get('/',auth,(req, res)=>{
+router.get('/', jwtMiddleware,(req, res)=>{
+const user = req.decoded;
 
-try{
-return res.status(200).send({Name:req.session.Alumni.alumni_name
+    const name = user.alumni_name;
+    const id = user.alumni_id
+    if (name) {
+        res.send({ name: user.alumni_name, id:user.alumni_id,email:user.alumni_email,faculty:user.faculty})
+    } else {
+        res.status(500)
+    }
+/*try{ 
+  return res.status(200).send({Name:req.session.Alumni.alumni_name
   ,Surname:req.session.Alumni.alumni_surname,Email:req.session.Alumni.alumni_email,Id:req.session.Alumni.alumni_id,faculty:req.session.Alumni.faculty });
 }catch(err){
 return res.status(404).send("Note!! ,Please Login to view Profile!! \n"+err);
-}
+}*/
 
 
 });
